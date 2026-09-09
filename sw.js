84a3722ec69ee97d991df740e6e152eac22aa964
Alexliew829 <alexliew2026@gmail.com>
2026-09-09 22:48:54 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// V5.6 legacy service-worker cleanup.
+// V5.7 legacy service-worker cleanup.
 // This app no longer uses offline page caching so Home Screen always loads the latest deployment.
 self.addEventListener("install", function () {
   self.skipWaiting();
